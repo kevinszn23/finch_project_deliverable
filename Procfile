cbfc6faada25e44e99ac6ea50bf04dbb79d2f7a7
Kevin Ng <kehfunn@gmail.com>
2022-10-06 17:04:31 -0400

    deploying on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn finch-collector-sei725.wsgi
--- finch_collector/settings.py
+++ finch_collector/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
+import django_heroku
+django_heroku.settings(locals())
